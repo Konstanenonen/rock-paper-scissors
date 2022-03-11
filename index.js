b701d06738f5f354a371f762b0b5616cc6532b3d
Konsta Nenonen <konstanenonen@protonmail.com>
2022-03-11 21:42:59 +0200

    Big improvements to the look of the game

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ let playerScore = 0;
 let computerScore = 0;
 let gameOver = false;
 
-const playButtons = document.querySelectorAll("button");
+const playButtons = document.querySelectorAll(".card");
 playButtons.forEach(button => button.addEventListener("click", () => game(button)));
 
 function game(button) {
   playRound(button.innerText, computerPlay());
-  document.querySelector(".score-count").innerText = `Your score: ${playerScore} Computer score: ${computerScore}`;
+  document.querySelector(".score-count-player").innerText = `You: ${playerScore}`;
+  document.querySelector(".score-count-computer").innerText = `Computer: ${computerScore}`;
 
-  const winningText = document.createElement("h2");
+  const winningText = document.createElement("h3");
   const resultsArea = document.querySelector(".results-area");
 
   if (playerScore === 5 && !gameOver) {
-    winningText.innerText = `You have defeated the computer!`;
+    winningText.innerText = `You are the ultimate winner!`;
     winningText.setAttribute("class", "win-text win");
     resultsArea.appendChild(winningText);
     gameOver = true;
   } else if (computerScore === 5 && !gameOver) {
-    winningText.innerText = `Oh no! The computer bested you this time.`;
+    winningText.innerText = `The computer has won the game...`;
     winningText.setAttribute("class", "win-text lose");
     resultsArea.appendChild(winningText);
     gameOver = true;
@@ -27,7 +28,8 @@ function game(button) {
     resultsArea.removeChild(childToBeRemoved);
     playerScore = 0;
     computerScore = 0;
-    document.querySelector(".score-count").innerText = `Your score: ${playerScore} Computer score: ${computerScore}`;
+    document.querySelector(".score-count-player").innerText = `You: ${playerScore}`;
+  document.querySelector(".score-count-computer").innerText = `Computer: ${computerScore}`;
     document.querySelector(".results").innerText = "";
     gameOver = false;
   }
@@ -50,11 +52,14 @@ function playRound(playerSelection, computerSelection) {
 
   if (playerSelection === computerSelection) {
     resultsArea.innerText = `It's a tie! ${playerSelection} ties with ${computerSelection}`;
+    resultsArea.style = "color: black;";
   } else if (winningPairs[playerSelection] === computerSelection) {
     resultsArea.innerText = `You win! ${playerSelection} beats ${computerSelection}`;
+    resultsArea.style = "color: green;";
     playerScore++;
   } else {
     resultsArea.innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
+    resultsArea.style = "color: red;";
     computerScore++;
   }
 }
