a25494643915a4bd50d070eb9e925813980668c1
Konsta Nenonen <konstanenonen@protonmail.com>
2022-03-16 23:37:54 +0200

    Improved the look and added play again button.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 let playerScore = 0;
 let computerScore = 0;
-let gameOver = false;
 
-const playButtons = document.querySelectorAll(".card");
-playButtons.forEach(button => button.addEventListener("click", () => game(button)));
+addListenersToButtons();
 
 function game(button) {
   playRound(button.innerText, computerPlay());
@@ -13,25 +11,28 @@ function game(button) {
   const winningText = document.createElement("h3");
   const resultsArea = document.querySelector(".results-area");
 
-  if (playerScore === 5 && !gameOver) {
+  if (playerScore === 5) {
     winningText.innerText = `You are the ultimate winner!`;
     winningText.setAttribute("class", "win-text win");
     resultsArea.appendChild(winningText);
-    gameOver = true;
-  } else if (computerScore === 5 && !gameOver) {
+    const playAgaingButton = document.createElement("button");
+    playAgaingButton.innerText = "Play Again";
+    playAgaingButton.setAttribute("class", "play-again-button");
+    const buttonArea = document.querySelector(".container");
+    buttonArea.innerHTML = "";
+    playAgaingButton.addEventListener("click", startNewGame);
+    buttonArea.appendChild(playAgaingButton);
+  } else if (computerScore === 5) {
     winningText.innerText = `The computer has won the game...`;
     winningText.setAttribute("class", "win-text lose");
     resultsArea.appendChild(winningText);
-    gameOver = true;
-  } else if (gameOver) {
-    const childToBeRemoved = document.querySelector(".win-text");
-    resultsArea.removeChild(childToBeRemoved);
-    playerScore = 0;
-    computerScore = 0;
-    document.querySelector(".score-count-player").innerText = `You: ${playerScore}`;
-  document.querySelector(".score-count-computer").innerText = `Computer: ${computerScore}`;
-    document.querySelector(".results").innerText = "";
-    gameOver = false;
+    const playAgaingButton = document.createElement("button");
+    playAgaingButton.innerText = "Play Again";
+    playAgaingButton.setAttribute("class", "play-again-button");
+    const buttonArea = document.querySelector(".container");
+    buttonArea.innerHTML = "";
+    playAgaingButton.addEventListener("click", startNewGame);
+    buttonArea.appendChild(playAgaingButton);
   }
 }
 
@@ -62,4 +63,34 @@ function playRound(playerSelection, computerSelection) {
     resultsArea.style = "color: red;";
     computerScore++;
   }
+}
+
+function startNewGame() {
+  document.querySelector(".container").innerHTML = `<div class="container">
+    <div class="card">
+      <img src="./assets/images/rock.jpg">
+      <p class="card-title">Rock</p>
+    </div>
+    <div class="card">
+      <img src="./assets/images/paper.jpg">
+      <p class="card-title">Paper</p>
+    </div>
+    <div class="card">
+      <img src="./assets/images/scissors.jpg">
+      <p class="card-title">Scissors</p>
+    </div>
+  </div>`;
+  addListenersToButtons();
+  playerScore = 0;
+  computerScore = 0;
+  document.querySelector(".score-count-player").innerText = `You: ${playerScore}`;
+  document.querySelector(".score-count-computer").innerText = `Computer: ${computerScore}`;
+  document.querySelector(".results").innerText = "";
+  const winText = document.querySelector(".win-text");
+  document.querySelector(".results-area").removeChild(winText);
+}
+
+function addListenersToButtons() {
+  const playButtons = document.querySelectorAll(".card");
+  playButtons.forEach(button => button.addEventListener("click", () => game(button)));
 }
